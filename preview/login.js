53f05db43fd9db7032b3a6abf5abc39af6b95543
PyeonRa <Brian3155@naver.com>
2023-12-02 12:30:00 +0900

    preview 추가

---
+++ preview/login.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded",
+
+    function(e) {
+        
+
+        const usernameInput = document.querySelector('input[name="Username"]');
+        const passwordInput = document.querySelector('input[name="Password"]');
+        const form = document.getElementById('box');
+        const usernameErrorDiv = createErrorDiv('usernameError');
+        const passwordErrorDiv = createErrorDiv('passwordError');
+
+        usernameInput.addEventListener('input', () => validateInput(usernameInput, usernameErrorDiv, /^[a-zA-Z0-9]{8,}$/, "유효한 사용자 이름을 입력해주세요"));
+        passwordInput.addEventListener('input', () => validateInput(passwordInput, passwordErrorDiv, /^[a-zA-Z0-9!@#$%^&*()_+]{8,}$/, "유효한 비밀번호를 입력해주세요"));
+
+        form.addEventListener('submit', function(event) {
+            console.log("폼 제출 시도");
+            const isUsernameValid = validateInput(usernameInput, usernameErrorDiv, /^[a-zA-Z0-9]{8,}$/, "유효한 사용자 이름을 입력해주세요");
+            const isPasswordValid = validateInput(passwordInput, passwordErrorDiv, /^[a-zA-Z0-9!@#$%^&*()_+]{8,}$/, "유효한 비밀번호를 입력해주세요");
+    
+            if (!isUsernameValid || !isPasswordValid) {
+                event.preventDefault();
+            }
+        });
+
+        function validateInput(inputElement, errorDiv, regex, errorMessage) {
+            if (!regex.test(inputElement.value)) {
+                errorDiv.textContent = errorMessage;
+                if (!document.getElementById(errorDiv.id)) {
+                    inputElement.after(errorDiv);
+                }
+                return false;
+            } else {
+                errorDiv.textContent = '';
+                return true;
+            }
+        }
+
+        function createErrorDiv(id) {
+            const div = document.createElement('div');
+            div.id = id;
+            div.style.color = 'red';
+            div.style.margin = '5px';
+            return div;
+        }
+
+
+    }
+)
