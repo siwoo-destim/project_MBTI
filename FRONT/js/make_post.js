b9bf78d4ff353a28bc0f8f29a4f672a00cc90ce3
PyeonRa <Brian3155@naver.com>
2023-12-28 23:26:55 +0900

    수정

--- FRONT/js/make_post.js
+++ FRONT/js/make_post.js
@@ -1,38 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-
     var imageInput = document.getElementById("image-upload");
     var previewImage = document.getElementById("previewImage");
 
     imageInput.addEventListener("change", function () {
+        var file = imageInput.files[0];
 
-      var file = imageInput.files[0];
+        if (file) {
+            var reader = new FileReader();
 
-      if (file) {
+            reader.onload = function (e) {
+                previewImage.src = e.target.result;
+                previewImage.style.display = "block";
+            };
 
-        var reader = new FileReader();
-
-        reader.onload = function (e) {
-
-          previewImage.src = e.target.result;
-
-          previewImage.style.display = "block";
-        };
-
-        reader.readAsDataURL(file);
-      }
+            reader.readAsDataURL(file);
+        }
     });
 
     const MbtiNameInput = document.querySelector('input[name="request_mbti_name"]');
     const form = document.getElementById('box');
     const MbtiNameErrorDiv = createErrorDiv('MbtiNameError');
 
+
     MbtiNameInput.addEventListener('input', () => validateInput(MbtiNameInput, MbtiNameErrorDiv, /^[a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9]{1,16}$/, "유효한 이름을 입력해주세요 (16자 이하의 한국어, 영어, 숫자)"));
 
     form.addEventListener('submit', function(event) {
         const isNameValid = validateInput(MbtiNameInput, MbtiNameErrorDiv, /^[a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9]{1,16}$/, "유효한 이름을 입력해주세요 (16자 이하의 한국어, 영어, 숫자)");
 
-        if (!isNameValid) {
+        // 이미지 업로드 여부 확인
+        if (!imageInput.files.length) {
+            event.preventDefault(); // 폼 제출 중단
+            if (confirm("사진을 찍지 않겠습니까?")) {
+                // 사용자가 확인을 눌렀을 때만 폼을 다시 제출
+                imageInput.required = true;
+                form.submit();
+            }
+        } else if (!isNameValid) {
+            // 이름 입력 검증이 실패했을 경우 폼 제출 중단
             event.preventDefault();
         }
     });
@@ -57,5 +61,4 @@ document.addEventListener('DOMContentLoaded', function () {
         div.style.margin = '5px';
         return div;
     }
-    
 });
